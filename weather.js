eea69044acabdd18fc8dd43d9f0d1dc8b75f9bfd
Madumere Maryann <maryannmadumere18@gmail.com>
2025-10-05 21:06:47 +0100

    latest updates

--- weather.js
+++ weather.js
@@ -7,6 +7,7 @@ const errorMsg = document.getElementById("errorMsg");
  async function getWeather(city) {
    try {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
     const res = await fetch(url);
 
     if (!res.ok) {
